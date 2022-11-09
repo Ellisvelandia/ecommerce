1da5e7dbc712e5944a1f50cbcdfab8e5eaddaa73
ellisvelandia <eyis619@gmail.com>
2022-11-08 20:38:06 -0500

    sanity

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,23 +1,34 @@
 import React from "react";
+import { client } from "../lib/client";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
-import { Product, FooterBanner, HeroBanner } from '../components';
-
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData}/>
       <div className="products-heading">
         <h2>Beset Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className="products-container">
-        {["Product 1 ", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "product"]';
+  const bannerData = await client.fetch(bannerQuery);
+  return {
+    props: { products, bannerData }, // will be passed to the page component as props
+  };
+}
 
 export default Home;
