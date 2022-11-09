d3661a6f537ddffa449c9643bf89d162dd9a2474
ellisvelandia <eyis619@gmail.com>
2022-11-08 21:12:48 -0500

    sanity

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import Link from "next/link";
+import { urlFor } from "../lib/client";
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
-      <div> 
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphones" className="hero-banner-image" />
-        
+      <div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headphones"
+          className="hero-banner-image"
+        />
+
         <div>
           <Link href="/product/ID">
             <button type="button">BUTTON TEXT</button>
